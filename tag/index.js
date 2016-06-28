0979adf96f6f0f8dfa90beb579c19eda17acd582
Jérémy Lal <kapouer@melix.org>
2016-06-29 00:38:47 +0200

    Check response headers for tags

--- tag/index.js
+++ tag/index.js
@@ -7,6 +7,11 @@ function cacheTagMw(req, res, next) {
 		if (res.statusCode >= 200 && res.statusCode < 300) {
 			var tags = req.get('X-Cache-Tag');
 			tags = tags && tags.split(',') || [];
+			var rtags = res._headers['x-cache-tag'];
+			(rtags && rtags.split(',') || []).forEach(function(tag) {
+				if (!~tags.indexOf(tag)) tags.push(tag);
+			});
+
 			if (req.method != "GET") tags = tags.map(function(tag) {
 				return '+' + tag;
 			});
