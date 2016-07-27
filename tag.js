5430fa78cc567e218b08581d2d88ddb87efb486d
Jérémy Lal <kapouer@melix.org>
2016-07-27 23:56:17 +0200

    Simplify again tags

--- tag.js
+++ tag.js
@@ -4,20 +4,23 @@ var headerTag = 'X-Cache-Tag';
 
 module.exports = function() {
 	var tags = Array.from(arguments);
+	var len = tags.length;
+	var incFn;
+	if (len && typeof tags[len - 1] == "function") {
+		incFn = tags.pop();
+	} else {
+		incFn = function(req) {
+			return req.method != "GET";
+		};
+	}
 
 	return function tagMw(req, res, next) {
-		var resTags = res.get(headerTag) || [];
+		var inc = incFn(req);
 		tags.forEach(function(tag) {
-			if (!~resTags.indexOf(tag)) resTags.push(tag);
+			if (inc) tag = '+' + tag;
+			res.append(headerTag, tag);
 		});
-		// unicode string comparison
-		resTags.sort();
-
-		if (req.method != "GET") resTags = resTags.map(function(tag) {
-			return '+' + tag;
-		});
-		debug("response tags", resTags);
-		if (resTags.length) res.set(headerTag, resTags);
+		debug("response tags", res.get(headerTag));
 		if (next) next();
 	};
 };
