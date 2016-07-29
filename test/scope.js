b8b0ca7712159faad939c7e3f3b0451d9dae64e1
Jérémy Lal <kapouer@melix.org>
2016-07-29 15:02:38 +0200

    Test handshake removal

--- test/scope.js
+++ test/scope.js
@@ -211,6 +211,7 @@ describe("Scope", function suite() {
 			headers.Cookie = cookie.serialize("bearer", cookies.bearer);
 			return runner.get(req);
 		}).then(function(res) {
+			res.headers.should.not.have.property('x-cache-key-handshake');
 			res.headers.should.have.property('x-cache-scope', 'bookReader, bookSecond');
 			res.statusCode.should.equal(200);
 			count(req).should.equal(1);
