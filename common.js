bc9bcb117826957bbf30b61776150cee49ba1c7f
Jérémy Lal <kapouer@melix.org>
2016-08-02 17:25:09 +0200

    Fix params replacement

--- common.js
+++ common.js
@@ -1,10 +1,10 @@
 exports.replacements = function replacements(tag, params) {
-	return tag.replace(/\/:(\w+)/g, function(str, name) {
+	return tag.replace(/:(\w+)/g, function(str, name) {
 		var val = params[name];
 		if (val !== undefined) {
-			return '/' + val;
+			return val;
 		} else {
-			return '/:' + name;
+			return ':' + name;
 		}
 	});
 };
