976e968a570d8bb9f60323d70e2e8fc087db505b
Jérémy Lal <kapouer@melix.org>
2022-12-12 00:34:07 +0100

    Map and Vary test

--- test/map.js
+++ test/map.js
@@ -1,5 +1,6 @@
 const URL = require('url');
 const express = require('express');
+const { strict: assert } = require('assert');
 
 const runner = require('../lib/spawner');
 const common = require('./common');
@@ -16,7 +17,10 @@ const ports = {
 describe("Map", () => {
 	let servers, app;
 	const testPath = '/map-test';
-	const mappedTestPath = testPath + '-mapped';
+	const mappedTestPath = `${testPath}-mapped`;
+	const testVary = "/map-vary";
+	const mappedTestVary = `${testVary}-mapped`;
+
 	let counters = {};
 
 	function count(uri, inc) {
@@ -48,6 +52,25 @@ describe("Map", () => {
 				date: new Date()
 			});
 		});
+
+		app.get(testVary, tag('app'), (req, res, next) => {
+			map(res, mappedTestVary);
+			res.vary('Accept-Language');
+			res.append('Vary', 'Sec-Purpose');
+			count(req, 1);
+			const langs = ['en', 'fr'];
+			const suffix = req.get('Sec-Purpose') ?? '';
+			if (req.acceptsLanguages(langs) == 'en') {
+				res.set('Content-Language', 'en');
+				res.send('Good !' + suffix);
+			} else if (req.acceptsLanguages(langs) == 'fr') {
+				res.set('Content-Language', 'fr');
+				res.send('Bien !' + suffix);
+			} else {
+				res.set('Content-Language', 'pt');
+				res.send('Bem !' + suffix);
+			}
+		});
 	});
 
 	after(async () => {
@@ -70,13 +93,82 @@ describe("Map", () => {
 		};
 		let res = await common.get(req);
 		const result = res.body.toString();
+
 		res = await common.get(req);
-		res.body.toString().should.equal(result);
-		count(req).should.equal(1);
-		res.headers.should.have.property('x-upcache-map', mappedTestPath);
+		assert.equal(res.body.toString(), result);
+		assert.equal(count(req), 1);
+		assert.equal(res.headers['x-upcache-map'], mappedTestPath);
+
 		res = await common.get(reqm);
-		res.body.toString().should.equal(result);
-		count(req).should.equal(1);
-		res.headers.should.have.property('x-upcache-map', mappedTestPath);
+		assert.equal(res.body.toString(), result);
+		assert.equal(count(req), 1);
+		assert.equal(res.headers['x-upcache-map'], mappedTestPath);
+	});
+
+	it("and Vary should map to two different keys", async () => {
+		const headers = {};
+		const req = {
+			headers: headers,
+			port: ports.ngx,
+			path: testVary
+		};
+		let res;
+		const english = "fr;q=0.8, en, pt";
+		const french = "fr;q=0.8, en;q=0.7, pt;q=0.5";
+
+		req.headers['Accept-Language'] = english;
+		res = await common.get(req);
+		assert.equal(res.headers.vary, 'Accept-Language, Sec-Purpose');
+		assert.equal(res.headers['content-language'], 'en');
+		assert.equal(res.headers['x-upcache-map'], mappedTestVary);
+		assert.equal(count(req), 1);
+
+		req.headers['Accept-Language'] = french;
+		res = await common.get(req);
+		assert.equal(res.headers.vary, 'Accept-Language, Sec-Purpose');
+		assert.equal(res.headers['content-language'], 'fr');
+		assert.equal(res.headers['x-upcache-map'], mappedTestVary);
+		assert.equal(count(req), 2);
+
+		req.headers['Sec-Purpose'] = 'prerender';
+		res = await common.get(req);
+		assert.equal(res.body.toString(), 'Bien !prerender');
+		assert.equal(res.headers.vary, 'Accept-Language, Sec-Purpose');
+		assert.equal(res.headers['content-language'], 'fr');
+		assert.equal(res.headers['x-upcache-map'], mappedTestVary);
+		assert.equal(count(req), 3);
+		delete req.headers['Sec-Purpose'];
+
+		req.headers['Accept-Language'] = english;
+		res = await common.get(req);
+		assert.equal(res.headers.vary, 'Accept-Language, Sec-Purpose');
+		assert.equal(res.headers['content-language'], 'en');
+		assert.equal(res.headers['x-upcache-map'], mappedTestVary);
+		assert.equal(count(req), 3);
+
+		req.headers['Accept-Language'] = french;
+		res = await common.get(req);
+		assert.equal(res.body.toString(), 'Bien !');
+		assert.equal(res.headers.vary, 'Accept-Language, Sec-Purpose');
+		assert.equal(res.headers['content-language'], 'fr');
+		assert.equal(res.headers['x-upcache-map'], mappedTestVary);
+		assert.equal(count(req), 3);
+
+		req.headers['Sec-Purpose'] = 'prerender';
+		res = await common.get(req);
+		assert.equal(res.body.toString(), 'Bien !prerender');
+		assert.equal(res.headers.vary, 'Accept-Language, Sec-Purpose');
+		assert.equal(res.headers['content-language'], 'fr');
+		assert.equal(res.headers['x-upcache-map'], mappedTestVary);
+		assert.equal(count(req), 3);
+		delete req.headers['Sec-Purpose'];
+
+		req.headers['Accept-Language'] = "fr;q=0.8, en;q=0.9"; // another english
+		res = await common.get(req);
+		assert.equal(res.headers.vary, 'Accept-Language, Sec-Purpose');
+		assert.equal(res.headers['content-language'], 'en');
+		assert.equal(res.headers['x-upcache-map'], mappedTestVary);
+		assert.equal(count(req), 4);
+
 	});
 });
