11aeef8b375dd2cb587e99080819cd81374588fc
Jérémy Lal <kapouer@melix.org>
2023-10-15 15:53:01 +0200

    Rename test/host to test/cache

--- test/cache.js
+++ test/cache.js
@@ -1,11 +1,10 @@
-const URL = require('url');
 const express = require('express');
 const { strict: assert } = require('assert');
 
 const runner = require('../lib/spawner');
 const common = require('./common');
 
-const tag = require('..').tag;
+const tag = require('../lib').tag;
 
 const ports = {
 	app: 3000,
@@ -13,9 +12,10 @@ const ports = {
 	memc: 3002
 };
 
-describe("Host", () => {
+describe("Cache", () => {
 	let servers, app;
 	const testPath = '/map-host';
+	const cachePath = '/nocache'
 
 	let good = 0;
 	let bad = 0;
@@ -30,6 +30,13 @@ describe("Host", () => {
 		app.get(testPath, tag('test').for('1d'), (req, res, next) => {
 			if (req.get('Host') == app.fakeHost) good++;
 			else bad++;
+			res.send({
+				value: (req.path || '/').substring(1),
+				date: new Date()
+			});
+		});
+
+		app.get(cachePath, tag.disable(), tag.for('1d'), (req, res, next) => {
 			res.send({
 				value: (req.path || '/').substring(1),
 				date: new Date()
@@ -68,7 +75,24 @@ describe("Host", () => {
 		});
 		assert.equal(good, 1);
 		assert.equal(bad, 1);
+	});
 
+	it("nocache even if another middleware tries to", async () => {
+		const req = {
+			port: ports.ngx,
+			path: cachePath
+		};
+
+		let res = await common.get(req);
+
+		const result = res.body.toString();
+
+		res = await common.get(req);
+		assert.equal(res.body.toString(), result);
+		res = await common.get({
+			...req, headers: {}
+		});
+		assert.equal(res.headers['cache-control'], 'no-cache, must-revalidate, proxy-revalidate');
 	});
 
 });
